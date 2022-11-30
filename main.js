5d0f950f921aa00665bc0e1e7b1988f05d71032f
mohamed <mohamed.hesham.2049@gmail.com>
2022-11-30 15:43:14 +0200

    feat: :lipstick: add button styles and hover effects

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ import "./style.scss";
 function sum(num1, num2) {
   console.log(num1 + num2);
 }
-sum(8, 8);
 function subtract(num1, num2) {
   console.log(num1 - num2);
 }
-subtract(8, 8);
 
 function multiply(num1, num2) {
   console.log(num1 * num2);
 }
-multiply(8, 8);
 
 function divide(num1, num2) {
   console.log(num1 / num2);
 }
-divide(8, 8);
+
+function modulus(num1, num2) {
+  console.log(num1 % num2);
+}
 
 function operate(num1, operator, num2) {
   return operator(num1, num2);
 }
-operate(5, multiply, 5);
+operate(7, modulus, 5);
