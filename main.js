9d6c601cf445e5e1a899e18d01418ba8a44c9063
mohamed <mohamed.hesham.2049@gmail.com>
2022-11-30 06:15:33 +0200

    feat: :sparkles: add calculator UI

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import "./style.scss";
+
+function sum(num1, num2) {
+  console.log(num1 + num2);
+}
+sum(8, 8);
+function subtract(num1, num2) {
+  console.log(num1 - num2);
+}
+subtract(8, 8);
+
+function multiply(num1, num2) {
+  console.log(num1 * num2);
+}
+multiply(8, 8);
+
+function divide(num1, num2) {
+  console.log(num1 / num2);
+}
+divide(8, 8);
+
+function operate(num1, operator, num2) {
+  return operator(num1, num2);
+}
+operate(5, multiply, 5);
