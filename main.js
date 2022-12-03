738c53fb2a1809cb0f1b3147c14e6ada69c70927
mohamed <mohamed.hesham.2049@gmail.com>
2022-12-03 02:06:17 +0200

    feat: :sparkles: add clear functions

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ function displayNumbers() {
     }
   });
 }
+
+// numbers
 function displayExpression() {
   displayNumbers();
 
@@ -49,7 +51,7 @@ function operate(a, operator, b) {
   }
   if (operator == "-") {
     result = a - b;
-}
+  }
   if (operator == "%") {
     result = a % b;
   }
@@ -64,20 +66,26 @@ function calculate() {
       result.innerText = operate(oldValue, operator, currentValue);
       expression.innerText += currentValue;
       isCalculated = true;
-}
+    }
   });
 }
 calculate();
 
-function divide(num1, num2) {
-  console.log(num1 / num2);
+function clearExpression() {
+  expression.innerText = "";
 }
 
-function modulus(num1, num2) {
-  console.log(num1 % num2);
+function clearResult() {
+  result.innerText = "";
 }
 
-function operate(num1, operator, num2) {
-  return operator(num1, num2);
+function clearAll() {
+  clearExpression();
+  clearResult();
+  currentValue = "";
+  oldValue = "";
+  operator = "";
+  isCalculated = false;
 }
-operate(7, modulus, 5);
+
+// \xa0
